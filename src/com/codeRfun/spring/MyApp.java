b2022fbe83eaa5587c323643fec203df94f57142
Ker Tenepere <ker10epere@gmail.com>
2022-01-24 11:21:54 +0800

    configure tenniscoach for bean scope and lifecycle

--- src/com/codeRfun/spring/MyApp.java
+++ src/com/codeRfun/spring/MyApp.java
@@ -9,7 +9,12 @@ public class MyApp {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		Coach coach = context.getBean("tennisCoach", Coach.class);
-		System.out.println(coach.getDailyWorkout());
+		Coach coach2 = context.getBean("tennisCoach", Coach.class);
+		
+		boolean sameReference = coach == coach2;
+		System.out.println("Same Reference = " + sameReference);
+		System.out.println("C1 " + coach);
+		System.out.println("C2 " + coach2);
 	}
 
 }
--- src/com/codeRfun/spring/models/TennisCoach.java
+++ src/com/codeRfun/spring/models/TennisCoach.java
@@ -1,9 +1,14 @@
 package com.codeRfun.spring.models;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.codeRfun.spring.interfaces.Coach;
 
+@Scope("prototype")
 @Component("tennisCoach")
 public class TennisCoach implements Coach {
 
@@ -11,5 +16,16 @@ public class TennisCoach implements Coach {
 	public String getDailyWorkout() {
 		return "swing 20 times";
 	}
-
+	
+	// need to import  javax.annotation-api-1.3.2.jar or newer
+	// if using java 9 higher, for @PostContruct and @PreDestroy
+	@PostConstruct
+	public void init() {
+		System.out.println("initializing");
+	}
+	
+	@PreDestroy
+	public void destroy() {
+		System.out.println("cleaning up");
+	}
 }
