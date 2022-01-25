0ba76ea6fbf7764a2929a87e023096b1eaceca8b
Ker Tenepere <ker10epere@gmail.com>
2022-01-25 11:19:21 +0800

    add RequestMapping Controller and JSP contextPath

--- src/main/java/com/codingRfun/spring/HelloWorldController.java
+++ src/main/java/com/codingRfun/spring/HelloWorldController.java
@@ -1,30 +1,29 @@
 package com.codingRfun.spring;
 
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@RequestMapping("/funny")
 @Controller
 public class HelloWorldController {
 	@GetMapping("/showForm")
 	public String showForm() {
 		return "helloworld-form";
 	}
-	
+
 	@PostMapping("/processForm")
 	public String processForm() {
 		return "helloworld";
 	}
-	
+
 	@PostMapping("/processFormShout")
-	public String letsShoutDude(HttpServletRequest request, Model model) {
-		String name = request.getParameter("name");
+	public String letsShoutDude(@RequestParam String name, Model model) {
 		model.addAttribute("name", name.toUpperCase());
 		return "helloworld-shout";
 	}
-	
+
 }
